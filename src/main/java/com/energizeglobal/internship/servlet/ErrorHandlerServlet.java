c096925afbfaa8256a328cd63db6226267a74bb0
Arman Mikayelovich <armanmikayelovich@gmail.com>
2020-01-11 01:19:04 +0400

    Servlet,Parameters,Arguments,Filter tested.

---
+++ src/main/java/com/energizeglobal/internship/servlet/ErrorHandlerServlet.java
@@ -0,0 +1,21 @@
+package com.energizeglobal.internship.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ErrorHandlerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Throwable exception = (Throwable) req.getAttribute("javax.servlet.error.exception");
+        resp.getWriter().println(exception.toString());
+        resp.getWriter().flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
